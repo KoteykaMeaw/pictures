7438aef4affaf09f874d1ceb9efa1c127b7e2198
Koteyka <131580914+KoteykaMeaw@users.noreply.github.com>
2023-05-26 18:52:18 +0700

    Add files via upload

---
+++ discord_bot_v2.py
@@ -0,0 +1,69 @@
+import discord
+import os
+import random
+from discord.ext import commands
+import requests
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+
+@bot.command()
+async def animals(ctx):
+    images = os.listdir('images')
+    img_name = random.choice(images)
+    with open(f'images/{img_name}', 'rb') as f:
+            picture = discord.File(f)
+    await ctx.send(file=picture)
+@bot.command()
+async def mem(ctx):
+    images = os.listdir('mems')
+    img_name = random.choice(images)
+    with open(f'mems/{img_name}', 'rb') as f:
+            picture = discord.File(f)
+    await ctx.send(file=picture)
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+@bot.command('duck')
+async def duck(ctx):
+    '''По команде duck вызывает функцию get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+
+def get_dog_image_url():    
+    url = 'https://random.dog/woof.json'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+@bot.command('dog')
+async def dog(ctx):
+    '''По команде dog вызывает функцию get_dog_image_url'''
+    image_url = get_dog_image_url()
+    await ctx.send(image_url)
+
+
+def get_fox_image_url():    
+    url = 'https://randomfox.ca/floof/'
+    res = requests.get(url)
+    data = res.json()
+    return data['image']
+
+@bot.command('fox')
+async def fox(ctx):
+    '''По команде fox вызывает функцию get_fox_image_url'''
+    image_url = get_fox_image_url()
+    await ctx.send(image_url)
+bot.run('MTEwNDY3Mzc3MDQxMDYyNzEwMg.GrN6gi.E62bEgCyqh8Ftwf4nwVP6bcV9FiaE3jNyx4nZI')
